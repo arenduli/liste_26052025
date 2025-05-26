da171aa3525c2c430aa0968ee8a4e09d151ae7c8
anita <anita.rend@gmail.com>
2025-05-26 20:20:23 +0200

    solution for task on 26.05.2025.

---
+++ src/EvidencijaPolaznika.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+// pgm za evidenciju polaznika na tečaju
+public class EvidencijaPolaznika {
+
+    public static void main(String[] args) {
+        Set<Polaznik> polaznici = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Upišite 1 za dodavanje novog polaznika, ili upišite 2 za pregled svih polaznika, ili 0 za izlaz iz programa:");
+            int izbor = scanner.nextInt();
+            scanner.nextLine();
+
+            if (izbor == 0) {
+                break;
+            } else if (izbor == 1) {
+                System.out.print("Upišite ime: ");
+                String ime = scanner.nextLine();
+                System.out.print("Upišite prezime: ");
+                String prezime = scanner.nextLine();
+                System.out.print("Upišite e-mail: ");
+                String email = scanner.nextLine();
+
+                Polaznik noviPolaznik = new Polaznik(ime, prezime, email);
+
+                if (!polaznici.add(noviPolaznik)) {
+                    System.out.println("Polaznik s tim e-mailom već postoji!");
+                } else {
+                    System.out.println("Novi polaznik je dodan!");
+                }
+            } else if (izbor == 2) {
+                System.out.println("Svi polaznici na tečaju:");
+                // stream() metoda za sortiranje polaznika po prezimenu
+                polaznici.stream()
+                        .sorted((p1, p2) -> p1.getPrezime().compareToIgnoreCase(p2.getPrezime()))
+                        .forEach(System.out::println);
+            } else {
+                System.out.println("Pogrešan unos, pokušajte još jednom!");
+            }
+        }
+
+        scanner.close();
+    }
+}
